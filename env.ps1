c227d1335bf99f268f92db71297af71a116c499c
lpr <948392651@qq.com>
2025-12-22 15:27:53 +0800

    upd gradle

--- env.ps1
+++
@@ -1,8 +0,0 @@
-Get-Content .env1.local | ForEach-Object {
-    if ($_ -match "^\s*#") { return } # 忽略注释行
-    if ($_ -match "^\s*$") { return } # 忽略空行
-    $line = $_ -split "="
-    $key = $line[0].Trim()
-    $value = $line[1].Trim()
-    [System.Environment]::SetEnvironmentVariable($key, $value, "Process")
-}
